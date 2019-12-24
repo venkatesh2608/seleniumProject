00effa6c609a28a4ce8d09967e960833d198d159
erpdk5l <erpdk5l@MSPS-AUTO-041.na.corp.mckesson.com>
2019-12-24 12:31:16 +0530

    test

---
+++ src/testCases/BasicTestCase.java
@@ -0,0 +1,33 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import managers.WebDriverManager;
+
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+
+public class BasicTestCase {
+	public WebDriver driver = null;
+	WebDriverManager wdm = null;
+
+	@BeforeMethod
+	  public void beforeMethod() {
+		wdm = new WebDriverManager();
+		driver = wdm.getDriver();
+		wdm.launchApplicationUrl();
+	  }
+	
+	@Test
+	  public void f() {
+		driver.navigate().to("https://www.google.com");
+		//test
+		
+	  }
+	
+	@AfterMethod
+	  public void afterMethod() {
+		wdm.closeDriver();
+	  }  
+}
